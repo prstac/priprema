4f94a9bf0ac27a9cf627ffc1d9b81d1c3b5a115e
josip <josip@laptop>
2025-03-12 20:05:47 +0100

    refactor to use ternary

--- src/Drugi.java
+++ src/Drugi.java
@@ -5,12 +5,9 @@ public class Drugi {
     public static void iscrtaj(int x, int y) {
         String os = String.valueOf('o').repeat(Math.max(0, y));
         String xs = String.valueOf('*').repeat(Math.max(0, y));
+
         for (int i = 0; i < x; i++) {
-            if ((i&1)==0) {
-                System.out.println(xs);
-            } else {
-                System.out.println(os);
-            }
+            System.out.println((i&1)==0 ? xs : os);
         }
     }
 }
