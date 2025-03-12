36f0a75eed2f22a4b0b056d7f282eca1449dbecf
josip <josip@laptop>
2025-03-12 19:51:14 +0100

    refactor

--- src/Drugi.java
+++ src/Drugi.java
@@ -3,15 +3,14 @@ public class Drugi {
         iscrtaj(10,10);
     }
     public static void iscrtaj(int x, int y) {
+        String os = String.valueOf('o').repeat(Math.max(0, y));
+        String xs = String.valueOf('*').repeat(Math.max(0, y));
         for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
-                if((i&1) ==0) {
-                    System.out.print("*");
-                } else {
-                    System.out.print("o");
-                }
+            if ((i&1)==0) {
+                System.out.println(xs);
+            } else {
+                System.out.println(os);
             }
-            System.out.println();
         }
     }
 }
--- src/Treci.java
+++ src/Treci.java
@@ -1,15 +1,18 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Treci {
     public static void main(String[] args) {
         List<Polaznik> polaznici = new ArrayList<>();
+
         polaznici.add(new Polaznik(20, "Pero", "Peric"));
         polaznici.add(new Polaznik(19, "Mara", "Maric"));
         polaznici.add(new Polaznik(21, "Ivica", "Ivic"));
 
         Collections.sort(polaznici);
+
         for (Polaznik polaznik : polaznici) {
             System.out.println(polaznik);
         }
