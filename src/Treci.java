d047c39ded66fd77c961744977c4e5a67d9ba016
josip <josip@laptop>
2025-03-12 19:55:24 +0100

    random polaznici

--- src/Treci.java
+++ src/Treci.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Treci {
     public static void main(String[] args) {
         List<Polaznik> polaznici = new ArrayList<>();
 
-        polaznici.add(new Polaznik(20, "Pero", "Peric"));
-        polaznici.add(new Polaznik(19, "Mara", "Maric"));
-        polaznici.add(new Polaznik(21, "Ivica", "Ivic"));
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            polaznici.add(new Polaznik(random.nextInt(20,120),"ime"+i, "prezime"+i));
+        }
 
         polaznici.sort(Polaznik::compareTo);
         polaznici.forEach(System.out::println);
