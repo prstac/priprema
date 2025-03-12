7dd853fcd8d630331ecb40e9ff018a508eeecd0d
josip <josip@laptop>
2025-03-12 20:19:40 +0100

    remove unnecessary math max

--- src/Drugi.java
+++ src/Drugi.java
@@ -3,8 +3,8 @@ public class Drugi {
         iscrtaj(10,10);
     }
     public static void iscrtaj(int x, int y) {
-        String os = String.valueOf('o').repeat(Math.max(0, y));
-        String xs = String.valueOf('*').repeat(Math.max(0, y));
+        String os = String.valueOf('o').repeat(y);
+        String xs = String.valueOf('*').repeat(y);
 
         for (int i = 0; i < x; i++) {
             System.out.println((i&1)==0 ? xs : os);
